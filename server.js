4689e71d94fbe6cf32689fe32b25840493c40008
Mattia Falchi <falchimattia@icloud.com>
2025-07-02 01:09:11 +0100

    Fix: Overwrite all backedn files with known-good veriosns

--- server.js
+++ server.js
@@ -5,25 +5,22 @@ const { Pool } = require('pg');
 
 const app = express();
 
-//Create the DB pool once
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
 // Middleware
-app.options('*', cors()); // Handle preflight requests
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 
-// Import and use routes, passsing the pool to them
+// Import and use routes
 const authRoutes = require('./routes/auth')(pool);
 const userRoutes = require('./routes/users')(pool);
 
-app.use('/', authRoutes); // Handles /login, /forgot-password, etc.
-//app.use('/api', userRoutes); // Handles /api/register, /api/users, etc.
+app.use('/', authRoutes);
+app.use('/api', userRoutes);
 
-// Start the Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
